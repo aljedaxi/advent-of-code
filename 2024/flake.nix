f8c41e0d43a33cab3cd2cd17bb791135395eb327
jacob whitford-bender <jacob@xmentium.com>
2024-12-07 17:53:18 -0700

    feat: got stdin working

---
+++ 2024/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system  = "x86_64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        gerbil
+      ];
+      shellHook = ''exec zsh'';
+    };
+  };
+}
